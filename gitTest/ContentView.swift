569c754caead0febbce30cb866556d413fa7554a
testAcc-5963 <sasavo11@icloud.com>
2020-07-20 16:51:58 +0900

    Initial Commit

---
+++ gitTest/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  gitTest
+//
+//  Created by  Administrator on 2020/07/20.
+//  Copyright © 2020  Administrator. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, World!")
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
